7c2e9ae15f530783973b8ff0126f40f1cc4e92ca
mdw93 <mdwmdw9393@gmail.com>
2021-08-07 14:38:28 -0400

    updated views

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -1,3 +1,4 @@
+/*
 const checkoutEl = document.querySelector("#checkout");
 const userPayment = document.querySelector("#payment");
 
@@ -34,3 +35,46 @@ let rewardsToDollar = (points) => {
 
   return [points, dollarAmount];
 };
+*/
+
+creditCardButton = document.getElementById('creditCardButton')
+
+payInStoreButton = document.getElementById('payInStoreButton')
+
+creditCardInput = document.getElementById('creditCardInput')
+
+creditCardInputExtras = document.getElementById('creditCardInputExtras')
+
+pmtCancelCardButton = document.getElementById('pmtCancelCardButton')
+
+pmtCancelStoreButton = document.getElementById('pmtCancelStoreButton')
+
+creditCardButton.onclick = () => {
+    creditCardButton.setAttribute('style', "display: none")
+    payInStoreButton.setAttribute('style', "display: none")
+    creditCardInput.removeAttribute("style")
+    pmtCancelCardButton.removeAttribute("style")
+    creditCardInputExtras.removeAttribute("style")
+}
+
+pmtCancelCardButton.onclick = () => {
+    creditCardInput.setAttribute('style', "display: none")
+    pmtCancelCardButton.setAttribute('style', "display: none")
+    creditCardInputExtras.setAttribute('style', "display: none")
+    payInStoreButton.removeAttribute("style")
+    creditCardButton.removeAttribute("style")
+}
+
+payInStoreButton.onclick = () => {
+    creditCardButton.setAttribute('style', "display: none") 
+    payInStoreButton.setAttribute('style', "display: none") 
+    payInStore.removeAttribute("style") 
+    pmtCancelStoreButton.removeAttribute("style")
+}
+
+pmtCancelStoreButton.onclick = () => {
+    payInStore.setAttribute('style', "display: none")
+    pmtCancelStoreButton.setAttribute('style', "display: none")
+    payInStoreButton.removeAttribute("style")
+    creditCardButton.removeAttribute("style")
+}
