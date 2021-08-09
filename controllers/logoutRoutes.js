1d79666c262d8fa134732f72accc5490ece3cde7
mdw93 <mdwmdw9393@gmail.com>
2021-08-09 18:01:06 -0400

    logout

---
+++ controllers/logoutRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+router.post('/', (req, res) => {
+    if (req.session.logged_in) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
+  
+  module.exports = router;
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,11 +1,11 @@
 const logout = async () => {
-  const response = await fetch('/api/users/logout', {
+  const response = await fetch('/logout', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
   });
 
   if (response.ok) {
-    document.location.replace('/');
+    document.location.replace('/menu');
   } else {
     alert(response.statusText);
   }
