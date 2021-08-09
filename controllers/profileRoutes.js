2ed433cb78d85c34f87b7798fe7a38af1ca4f50e
eccentricality <youngmin13@gmail.com>
2021-08-08 21:22:13 -0400

    fixed error when attempting to load profile page without session login

--- controllers/profileRoutes.js
+++ controllers/profileRoutes.js
@@ -5,18 +5,20 @@ const router = require('express').Router();
 router.get('/', async (req, res) => {
   try {
     console.log("Does it work?")
-    const dbUserData = await User.findOne({ where: { id: req.session.userId} });
-    
-    console.log("Current Rewards Are:", dbUserData.rewards);
-    console.log("Current Name is:", dbUserData.firstName);
-    if (dbUserData) {
+    console.log(req.session.userId);
+    if (!req.session.userId) {
+      console.log("NO USER FOUND")
+      res.render('login');
+    } else {
+      const dbUserData = await User.findOne({ where: { id: req.session.userId} });
+
+      console.log("Current Rewards Are:", dbUserData.rewards);
+      console.log("Current Name is:", dbUserData.firstName);
       res.render('profile', {
         dbUserData,
         loggedIn: req.session.loggedIn
       });
-    } else {
-      res.render('login');
-    }
+    };
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
